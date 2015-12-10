36ef65eb3f42542a51954bb5fee28a8d02afcc23
Tommy Murphy <tmurphy@digitalocean.com>
2015-12-10 14:19:27 -0500

    Larger default instances

--- consul/vars.tf
+++ consul/vars.tf
@@ -2,7 +2,7 @@ variable "role" { default="consul" }
 variable "image" { default="ubuntu-14-04-x64" }
 variable "count" { default=3 }
 variable "region" { default="nyc2" }
-variable "size" { default="1gb" }
+variable "size" { default="2gb" }
 variable "ssh_key_id" {}
 variable "consul_encrypt_key" {}
 variable "ca_file" {}
--- vault/vars.tf
+++ vault/vars.tf
@@ -2,7 +2,7 @@ variable "role" { default="vault" }
 variable "image" { default="ubuntu-14-04-x64" }
 variable "count" { default=2 }
 variable "region" { default="nyc2" }
-variable "size" { default="1gb" }
+variable "size" { default="8gb" }
 variable "ssh_key_id" {}
 variable "consul_address" {}
 variable "consul_encrypt_key" {}
